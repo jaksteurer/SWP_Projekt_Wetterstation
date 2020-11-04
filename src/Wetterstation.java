dbb63bc9d49af7afdc8ac1c172d501b983a79314
jaksteurer <jaksteurer@tsn.at>
2020-11-04 10:38:19 +0100

    Datenverarbeitung

--- src/Wetterstation.java
+++ src/Wetterstation.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -9,54 +10,159 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+//Properties
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.util.Properties;
 
 import com.google.gson.Gson;
 
 public class Wetterstation {
 
-	static String dGetData = " ";
-	static String fcGetData = " ";
+	static String dGetData = "";
+	static String fcGetData = "";
 
 	public static void main(String[] args) throws IOException {
-		String ort = "Innsbruck";
-		//save current day json request in String
-		URL dUrl = new URL("http://api.openweathermap.org/data/2.5/weather?q="+ort+",at&units=metric&appid=793d753c4a6623defbfafdce3d337e9b");
-		HttpURLConnection dConn = (HttpURLConnection)dUrl.openConnection();
-		dConn.setRequestMethod("GET");
-		dConn.connect();
-		int dResponsecode = dConn.getResponseCode();
-		System.out.println("Code: "+dResponsecode);
-		if(dResponsecode != 200) {
-			throw new RuntimeException("HttpResonseCode: "+dResponsecode);
+		String defPlace = getConfigData();
+		//System.out.println(defPlace);
+		String place = requestPlace();
+		if (place.equals("d")) {
+			place = defPlace;
+			readCurrentWeatherAPI(place);
+			readForecastWeatherAPI(place);
+			showCurrentWeather();
+			showForecastWeather();
+		/*}if(place !=null) {
+			System.err.println("Wetter für den Ort "+place+":");
+			readCurrentWeatherAPI(place);
+			readForecastWeatherAPI(place);
+			currentWeather();
+			forecastWeather();*/
 		}else {
-			Scanner sc = new Scanner(dUrl.openStream());
-			while(sc.hasNext()) {
-				dGetData +=sc.nextLine();
-			}
-			//System.out.println(dInline);
-			sc.close();
-		}	
-		//save forecast weather json request in String
-		URL fcUrl = new URL("https://api.weatherbit.io/v2.0/forecast/daily?city="+ort+"&country=at&days=6&lang=de&key=1c3d135c16f640c1823ce502f303e586");
-		HttpURLConnection fcConn = (HttpURLConnection)fcUrl.openConnection();
-		fcConn.setRequestMethod("GET");
-		fcConn.connect();
-		int fcResponsecode = fcConn.getResponseCode();
-		//System.out.println("Code: "+fcResponsecode);
-		if(fcResponsecode != 200) {
-			throw new RuntimeException("HttpResonseCode: "+fcResponsecode);
-		}else {
-			Scanner sc = new Scanner(fcUrl.openStream());
-			while(sc.hasNext()) {
-				fcGetData +=sc.nextLine();
-			}
-			//System.out.println(fcInline);
-			sc.close();
+			//System.out.println("ungültiger Ort!");
+			System.err.println("Wetter für den Ort "+place+":");
+			readCurrentWeatherAPI(place);
+			readForecastWeatherAPI(place);
+			showCurrentWeather();
+			showForecastWeather();
 		}
-		currentWeather();
-		System.out.println();
-		forecastWeather();
+		//showCurrentWeather();
+		//forecastWeather();
 		//Datum(0);
+	}
+	@SuppressWarnings("unchecked")
+	public static void showCurrentWeather() {
+		System.out.println("------Current Wetter API------");
+		//aktuelles Wetter
+		try {
+			//Konvertieren des Json Strings "inline" in eine Java Map
+			Map<String, Object> dailyWeatherMap = new Gson().fromJson(dGetData, Map.class);
+			//Java Map ausgeben mit allen Werten und den Keys
+			/*for (Map.Entry<String, Object> key : dailyWeatherMap.entrySet()) {
+					System.out.println(key.getKey()+"="+key.getValue());
+					}*/
+			//Werte eines einzelnen Keys anzeigen lassen
+			//System.out.println("Main: "+ dailyWeatherMap.get("main"));
+			//Werte des Keys (falls Key mehr Werte hat) in eine weiteren Map speichern
+			//und davon wieder Map.get("Objektname") herausfiltern um genau den
+			//gewünschten Wert zu bekommen
+			Map<String, Object> main = (Map<String, Object>) dailyWeatherMap.get("main");
+			System.out.println("aktuelle Temperatur:\t\t"+main.get("temp")+" C°");
+			System.out.println("max. Temperatur:\t\t"+main.get("temp_max")+" C°");
+			System.out.println("min. Temperatur:\t\t"+main.get("temp_min")+" C°");
+			System.out.println("Luftfeuchtigkeit:\t\t"+main.get("humidity")+" %");
+			//Wert welcher für Wind benötigt wird steht noch ein einer weiteren Map in der main Map
+			Map<String, Object> wind = (Map<String, Object>) dailyWeatherMap.get("wind");
+			System.out.println("Wind:\t\t\t\t"+wind.get("speed")+" Km/h");
+		}catch (Exception e){
+			System.out.println("Fehler: "+e);//e.printStackTrace();
+			System.out.println("[currentWeather]Bitte überprüfen ob sinnvoller Ort eingegeben wurde!");
+		}
+	}
+	@SuppressWarnings("unchecked")
+	public static void showForecastWeather() {
+		System.out.println("------Forecast Wetter API------");
+		try {
+			//Konvertieren des Json Strings "inline" in eine Java Map
+			Map<String, Object> forecastWeatherMap = new Gson().fromJson(fcGetData, Map.class);
+			//Java Map ausgeben mit allen Werten und den Keys
+			//			for (Map.Entry<String, Object> key : forecastWeatherMap.entrySet()) {
+			//							System.out.println(key.getKey()+"="+key.getValue());
+			//							}
+			//Werte eines einzelnen Keys anzeigen lassen
+			//System.out.println("data: "+ forecastWeatherMap.get("data"));
+			/*
+			 * Werte des Keys (falls Key mehr Werte hat) in eine weiteren Map speichern
+			 * und davon wieder Map.get("Objektname") herausfiltern um genau den
+			 * gewünschten Wert zu bekommen
+			 */
+			List<Object> data = (List<Object>) forecastWeatherMap.get("data");
+			//System.out.println("data: "+data);
+			//today
+			/*Map<Object, Object>data1 = (Map<Object, Object>) data.get(0);
+			 *System.out.println("heute: ("+data1.get("datetime")+")");
+			 *System.out.println("\tMinimal Temperatur:\t"+data1.get("low_temp")+" C°");
+			 *System.out.println("\tMaximal Temperatur:\t"+data1.get("max_temp")+" C°");
+			 *System.out.println("\tLuftfeuchtigkeit:\t"+data1.get("rh")+" %");
+			 *System.out.println("\tNiederschlag:\t\t"+data1.get("pop")+" %");
+			 */
+			//forecast day 1
+			Map<Object, Object>data2 = (Map<Object, Object>) data.get(1);
+			System.out.println("Morgen: ("+data2.get("datetime")+")");
+			System.out.println("\tMinimal Temperatur:\t"+data2.get("low_temp")+" C°");
+			System.out.println("\tMaximal Temperatur:\t"+data2.get("max_temp")+" C°");
+			System.out.println("\tLuftfeuchtigkeit:\t"+data2.get("rh")+" %");
+			System.out.println("\tWind:\t\t\t"+Math.round((double) data2.get("wind_spd")*100)/100.+" Km/h");
+			System.out.println("\tNiederschlag:\t\t"+data2.get("pop")+" %");
+			//forecast day 2
+			Map<Object, Object>data3 = (Map<Object, Object>) data.get(2);
+			System.out.println("in 2 Tagen: ("+data3.get("datetime")+")");
+			System.out.println("\tMinimal Temperatur:\t"+data3.get("low_temp")+" C°");
+			System.out.println("\tMaximal Temperatur:\t"+data3.get("max_temp")+" C°");
+			System.out.println("\tLuftfeuchtigkeit:\t"+data3.get("rh")+" %");
+			System.out.println("\tWind:\t\t\t"+Math.round((double) data3.get("wind_spd")*100)/100.+" Km/h");
+			System.out.println("\tNiederschlag:\t\t"+data3.get("pop")+" %");
+			//forecast day 3
+			Map<Object, Object>data4 = (Map<Object, Object>) data.get(3);
+			System.out.println("in 3 Tagen: ("+data4.get("datetime")+")");
+			System.out.println("\tMinimal Temperatur:\t"+data4.get("low_temp")+" C°");
+			System.out.println("\tMaximal Temperatur:\t"+data4.get("max_temp")+" C°");
+			System.out.println("\tLuftfeuchtigkeit:\t"+data4.get("rh")+" %");
+			System.out.println("\tWind:\t\t\t"+Math.round((double) data4.get("wind_spd")*100)/100.+" Km/h");
+			System.out.println("\tNiederschlag:\t\t"+data4.get("pop")+" %");
+			//forecast day 4
+			Map<Object, Object>data5 = (Map<Object, Object>) data.get(4);
+			System.out.println("in 4 Tagen:("+data5.get("datetime")+")");
+			System.out.println("\tMinimal Temperatur:\t"+data5.get("low_temp")+" C°");
+			System.out.println("\tMaximal Temperatur:\t"+data5.get("max_temp")+" C°");
+			System.out.println("\tLuftfeuchtigkeit:\t"+data5.get("rh")+" %");
+			System.out.println("\tWind:\t\t\t"+Math.round((double) data5.get("wind_spd")*100)/100.+" Km/h");
+			System.out.println("\tNiederschlag:\t\t"+data5.get("pop")+" %");
+			//forecast day 5
+			Map<Object, Object>data6 = (Map<Object, Object>) data.get(5);
+			System.out.println("in 5 Tagen:("+data6.get("datetime")+")");
+			System.out.println("\tMinimal Temperatur:\t"+data6.get("low_temp")+" C°");
+			System.out.println("\tMaximal Temperatur:\t"+data6.get("max_temp")+" C°");
+			System.out.println("\tLuftfeuchtigkeit:\t"+data6.get("rh")+" %");
+			System.out.println("\tWind:\t\t\t"+Math.round((double) data6.get("wind_spd")*100)/100.+" Km/h");
+			System.out.println("\tNiederschlag:\t\t"+data6.get("pop")+" %");
+		}catch (Exception e){
+			System.out.println("Fehler: "+e);//e.printStackTrace();
+			System.out.println("[forecasttWeather]Bitte überprüfen ob sinnvoller Ort eingegeben wurde!");	
+		}
+	}
+	public static String requestPlace() {
+		String place=null;
+		try {	
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Ort in Österreich eingeben: ");
+			place = sc.next();
+			sc.close();
+		}catch (Exception e) {
+			System.out.println("existierenden Ort eingeben");
+		}	
+		return place;
+
 	}
 	public static void Datum(int datum) {
 		// Instanz vom Typ Kalender wird erstellt
@@ -92,97 +198,78 @@ public class Wetterstation {
 		default:System.out.println("Wert muss zwischen 0 und 5 liegen");
 		}
 	}
-	public static void currentWeather() {
-		System.out.println("------Current Wetter API------");
-		//aktuelles Wetter
+	public static String getConfigData() {
+		String configData = "C:\\Jakob\\Schule HTL Anichstraße\\Jakob Schule 5AHWII (20-21)\\SWP (Rubner)\\Projekt_Wetterstation\\Konfigurationsdatei.properties";
+		String defaultPlace = "";
 		try {
-			//Konvertieren des Json Strings "inline" in eine Java Map
-			Map<String, Object> dailyWeatherMap = new Gson().fromJson(dGetData, Map.class);
-			//Java Map ausgeben mit allen Werten und den Keys
-			/*for (Map.Entry<String, Object> key : dailyWeatherMap.entrySet()) {
-					System.out.println(key.getKey()+"="+key.getValue());
-					}*/
-			//Werte eines einzelnen Keys anzeigen lassen
-			//System.out.println("Main: "+ dailyWeatherMap.get("main"));
-			//Werte des Keys (falls Key mehr Werte hat) in eine weiteren Map speichern
-			//und davon wieder Map.get("Objektname") herausfiltern um genau den
-			//gewünschten Wert zu bekommen
-			Map<String, Object> main = (Map<String, Object>) dailyWeatherMap.get("main");
-			System.out.println("aktuelle Temperatur:\t\t"+main.get("temp")+" C°");
-			System.out.println("max. Temperatur:\t\t"+main.get("temp_max")+" C°");
-			System.out.println("min. Temperatur:\t\t"+main.get("temp_min")+" C°");
-			System.out.println("Luftfeuchtigkeit:\t\t"+main.get("humidity")+" %");
-			Map<String, Object> wind = (Map<String, Object>) dailyWeatherMap.get("wind");
-			System.out.println("Wind:\t\t\t\t"+wind.get("speed")+" Km/h");
-		}catch (Exception e){
-			e.printStackTrace();	
+			Properties properties = new Properties();
+			BufferedInputStream stream;
+			stream = new BufferedInputStream(new FileInputStream(configData));
+			properties.load(stream);
+			defaultPlace = properties.getProperty("defaultPlace");
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+		return defaultPlace;
 	}
-	public static void forecastWeather() {
-		//forecast Wetter
-		System.out.println("------Forecast Wetter API------");
+	public static void readCurrentWeatherAPI(String place) {
+		//save current day json request in String
+		URL dUrl;
 		try {
-			//Konvertieren des Json Strings "inline" in eine Java Map
-			Map<String, Object> forecastWeatherMap = new Gson().fromJson(fcGetData, Map.class);
-			//Java Map ausgeben mit allen Werten und den Keys
-//			for (Map.Entry<String, Object> key : forecastWeatherMap.entrySet()) {
-//							System.out.println(key.getKey()+"="+key.getValue());
-//							}
-			//Werte eines einzelnen Keys anzeigen lassen
-			//System.out.println("data: "+ forecastWeatherMap.get("data"));
-			/*
-			 * Werte des Keys (falls Key mehr Werte hat) in eine weiteren Map speichern
-			 * und davon wieder Map.get("Objektname") herausfiltern um genau den
-			 * gewünschten Wert zu bekommen
-			 */
-			List<Object> data = (List<Object>) forecastWeatherMap.get("data");
-			//System.out.println("data: "+data);
-			//forecast day 1
-			Map<Object, Object>data1 = (Map<Object, Object>) data.get(0);
-			System.out.println("heute: ("+data1.get("datetime")+")");
-			System.out.println("\tMinimal Temperatur:\t"+data1.get("low_temp")+" C°");
-			System.out.println("\tMaximal Temperatur:\t"+data1.get("max_temp")+" C°");
-			System.out.println("\tLuftfeuchtigkeit:\t"+data1.get("rh")+" %");
-			System.out.println("\tNiederschlag:\t\t"+data1.get("pop")+" %");
-			//forecast day 2
-			Map<Object, Object>data2 = (Map<Object, Object>) data.get(1);
-			System.out.println("Morgen: ("+data2.get("datetime")+")");
-			System.out.println("\tMinimal Temperatur:\t"+data2.get("low_temp")+" C°");
-			System.out.println("\tMaximal Temperatur:\t"+data2.get("max_temp")+" C°");
-			System.out.println("\tLuftfeuchtigkeit:\t"+data2.get("rh")+" %");
-			System.out.println("\tNiederschlag:\t\t"+data2.get("pop")+" %");
-			//forecast day 3
-			Map<Object, Object>data3 = (Map<Object, Object>) data.get(2);
-			System.out.println("in 2 Tagen: ("+data3.get("datetime")+")");
-			System.out.println("\tMinimal Temperatur:\t"+data3.get("low_temp")+" C°");
-			System.out.println("\tMaximal Temperatur:\t"+data3.get("max_temp")+" C°");
-			System.out.println("\tLuftfeuchtigkeit:\t"+data3.get("rh")+" %");
-			System.out.println("\tNiederschlag:\t\t"+data3.get("pop")+" %");
-			//forecast day 4
-			Map<Object, Object>data4 = (Map<Object, Object>) data.get(3);
-			System.out.println("in 3 Tagen: ("+data4.get("datetime")+")");
-			System.out.println("\tMinimal Temperatur:\t"+data4.get("low_temp")+" C°");
-			System.out.println("\tMaximal Temperatur:\t"+data4.get("max_temp")+" C°");
-			System.out.println("\tLuftfeuchtigkeit:\t"+data4.get("rh")+" %");
-			System.out.println("\tNiederschlag:\t\t"+data4.get("pop")+" %");
-			//forecast day 5
-			Map<Object, Object>data5 = (Map<Object, Object>) data.get(4);
-			System.out.println("in 4 Tagen:("+data5.get("datetime")+")");
-			System.out.println("\tMinimal Temperatur:\t"+data5.get("low_temp")+" C°");
-			System.out.println("\tMaximal Temperatur:\t"+data5.get("max_temp")+" C°");
-			System.out.println("\tLuftfeuchtigkeit:\t"+data5.get("rh")+" %");
-			System.out.println("\tNiederschlag:\t\t"+data5.get("pop")+" %");
-			Map<Object, Object>data6 = (Map<Object, Object>) data.get(5);
-			System.out.println("in 5 Tagen:("+data6.get("datetime")+")");
-			System.out.println("\tMinimal Temperatur:\t"+data6.get("low_temp")+" C°");
-			System.out.println("\tMaximal Temperatur:\t"+data6.get("max_temp")+" C°");
-			System.out.println("\tLuftfeuchtigkeit:\t"+data6.get("rh")+" %");
-			System.out.println("\tNiederschlag:\t\t"+data5.get("pop")+" %");
-		}catch (Exception e){
-			e.printStackTrace();	
+			dUrl = new URL("http://api.openweathermap.org/data/2.5/weather?q="+place+",at&units=metric&appid=793d753c4a6623defbfafdce3d337e9b");
+			HttpURLConnection dConn = (HttpURLConnection)dUrl.openConnection();
+			dConn.setRequestMethod("GET");
+			dConn.connect();
+			int dResponsecode = dConn.getResponseCode();
+			//System.out.println("Code: "+dResponsecode);
+			if(dResponsecode != 200) {
+				System.out.println("Bitte sinnvollen Ort eingeben!"); 
+				System.out.println("[DailyAPI]HttpResonseCode: "+dResponsecode);
+			}else {
+				Scanner sc = new Scanner(dUrl.openStream());
+				while(sc.hasNext()) {
+					dGetData +=sc.nextLine();
+				}
+				//System.out.println(dInline);
+				sc.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+
+	}
+	public static void readForecastWeatherAPI(String place) {
+		//save forecast weather json request in String
+		URL fcUrl;
+		try {
+			fcUrl = new URL("https://api.weatherbit.io/v2.0/forecast/daily?city="+place+"&country=at&days=6&lang=de&key=1c3d135c16f640c1823ce502f303e586");
+			HttpURLConnection fcConn = (HttpURLConnection)fcUrl.openConnection();
+			fcConn.setRequestMethod("GET");
+			fcConn.connect();
+			int fcResponsecode = fcConn.getResponseCode();
+			//System.out.println("Code: "+fcResponsecode);
+			if(fcResponsecode != 200) {
+				System.out.println("Bitte sinnvollen Ort eingeben!");
+				System.out.println("[ForecastAPI]HttpResonseCode: "+fcResponsecode);
+				System.out.println("Help for responsecodes: https://www.tutorialspoint.com/servlets/servlets-http-status-codes.htm");
+			}else {
+				Scanner sc = new Scanner(fcUrl.openStream());
+				while(sc.hasNext()) {
+					fcGetData +=sc.nextLine();
+				}
+				//System.out.println(fcInline);
+				sc.close();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	
+
+
+
 
 }
