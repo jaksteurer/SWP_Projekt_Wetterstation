05150ad831d13bbde9c3e272d5f7f0984923c6e7
jaksteurer <jaksteurer@tsn.at>
2021-03-24 08:35:09 +0100

    update

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main  extends Application {
+public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -21,8 +21,7 @@ public class Main  extends Application {
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
-		}
-		
+		}		
 	}
 
 	public static void main(String[] args) {
